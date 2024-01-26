cf0a28568d2a6f9c07919ce1b15f7dcfad6bedfe
adil-micromerger <a.shinwari@pk.micromerger.com>
2024-01-26 16:29:10 +0500

    request and response headers

---
+++ app.js
@@ -0,0 +1,21 @@
+const http = require("http");
+
+const server = http.createServer((req, res) => {
+    const url = req.url;
+    if(url === '/'){
+          
+    }
+  console.log(req.url);
+  console.log(req.method);
+  console.log(req.headers);
+  //   process.exit();
+
+  res.setHeader("Content-Type", "text/html");
+  res.write("<html>");
+  res.write("<head><title>My First Page</title></head>");
+  res.write("<body><h1>Hello Saifullah, from node.js server</h1></body>");
+  res.write("</html>");
+  res.end();
+});
+
+server.listen(3000);
